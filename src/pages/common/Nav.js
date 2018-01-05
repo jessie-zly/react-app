8995f3ac5948e224165c6cd08bc8444f0c7455ef
jessie-zly <610141748@qq.com>
2018-01-05 11:13:27 +0800

    add active of nav

--- src/pages/common/Nav.js
+++ src/pages/common/Nav.js
@@ -17,13 +17,13 @@ class Nav extends React.Component {
     return (
       <ul className="nav">
         <li className="nav-home">
-          <Link to="/home/film"> 首页</Link>
+          <Link to="/home/film" activeClassName='nav-active'> 首页</Link>
         </li>
         <li className="nav-order">
-          <Link to="/order">订单</Link>
+          <Link to="/order" activeClassName='nav-active'>票来</Link>
         </li>
         <li className="nav-my">
-          <Link to="/user">我的</Link>
+          <Link to="/user" activeClassName='nav-active'>我的</Link>
         </li>
       </ul>
     );
